ac49df920e09d615aaecd213230e82d1b611e370
DT Nesimi <ditinesimi@gmail.com>
2025-05-29 15:11:42 -0400

    sprite changes directions!

--- Lab6_SpriteCollision/reimu.cpp
+++ Lab6_SpriteCollision/reimu.cpp
@@ -47,21 +47,73 @@ void reimuClass::create_reimu_bitmap(ALLEGRO_DISPLAY* display)
 			x + 17, y + 17
 		};
 
-		switch (i)
-		{
-		case 0: //Up
-			al_draw_filled_triangle(x - 10, y - 10, x + 11, y - 10, x, y - 15, al_map_rgb(255, 0, 0));
-			break;
-		case 1://Right
-			al_draw_filled_triangle(x + 11, y - 11, x + 11, y + 11, x + 15, y, al_map_rgb(255, 0, 0));
-			break;
-		case 2://Down
-			al_draw_filled_triangle(x - 11, y + 11, x + 11, y + 11, x, y + 15, al_map_rgb(255, 0, 0));
-			break;
-		case 3: //Left
-			al_draw_filled_triangle(x - 11, y - 11, x - 11, y + 11, x - 15, y, al_map_rgb(255, 0, 0));
-			break;
+		if (i == 3 || i == 2) {
+			al_draw_filled_rectangle(x + 6, y - 4, x - 6, y + 10, al_map_rgb(255, 0, 0)); // body
+			al_draw_filled_circle(x, y - 10, 10, al_map_rgb(50, 50, 100)); //hair
+
+			al_draw_filled_triangle(x + 5, y + 24, x, y + 32, x, y + 24, al_map_rgb(255, 255, 255)); //left leg
+			al_draw_filled_triangle(x - 4, y + 24, x, y + 30, x, y + 24, al_map_rgb(130, 130, 130)); //right leg
+
+			al_draw_filled_triangle(x, y, x + 20, y + 20, x - 20, y + 20, al_map_rgb(255, 0, 0)); //dress
+			al_draw_filled_ellipse(x, y + 20, 16, 4, al_map_rgb(255, 0, 0)); // rounded dress bottom
+			al_draw_spline(dress_points, al_map_rgb(255, 255, 255), 1); // dress thread
+			al_draw_line(x + 6, y + 6, x - 5, y + 6, al_map_rgb(255, 255, 255), 2);//waist ribbon
+
+			al_draw_filled_triangle(x + 6, y - 4, x + 21, y, x + 18, y + 12, al_map_rgb(255, 255, 255)); //left arm
+			al_draw_filled_triangle(x - 6, y - 4, x - 21, y, x - 18, y + 12, al_map_rgb(255, 255, 255)); //right arm
+			al_draw_line(x + 20, y, x + 16, y + 10, al_map_rgb(255, 0, 0), 2); //left arm thread
+			al_draw_line(x - 20, y, x - 16, y + 10, al_map_rgb(255, 0, 0), 2); //right arm thread
+
+			al_draw_filled_triangle(x, y - 18, x + 16, y - 24, x + 12, y - 8, al_map_rgb(255, 0, 0)); //left ribbon
+			al_draw_filled_triangle(x, y - 18, x - 16, y - 24, x - 12, y - 8, al_map_rgb(255, 0, 0)); //right ribbon
+			al_draw_line(x + 16, y - 24, x + 12, y - 8, al_map_rgb(255, 255, 255), 2); //left ribbon frill
+			al_draw_line(x - 16, y - 24, x - 12, y - 8, al_map_rgb(255, 255, 255), 2); //right ribbon frill
+			al_draw_line(x + 2, y - 18, x - 2, y - 18, al_map_rgb(255, 0, 0), 3); //ribbon knot
+
+			al_draw_line(x - 20, y + 20, x - 28, y - 30, al_map_rgb(139, 69, 19), 2); //gohei stick
+			al_draw_filled_rectangle(x - 28, y - 30, x - 30, y - 25, al_map_rgb(255, 255, 255)); //gohei white pattern
+			al_draw_filled_rectangle(x - 30, y - 25, x - 32, y - 20, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 28, y - 20, x - 30, y - 15, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 30, y - 15, x - 32, y - 10, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 28, y - 10, x - 30, y - 5, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 30, y - 5, x - 32, y, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 28, y, x - 30, y + 5, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x - 30, y + 5, x - 32, y + 10, al_map_rgb(255, 255, 255));
 		}
+		else {
+			al_draw_filled_rectangle(x - 6, y - 4, x + 6, y + 10, al_map_rgb(255, 0, 0)); // body
+			al_draw_filled_circle(x, y - 10, 10, al_map_rgb(50, 50, 100)); //hair
+
+			al_draw_filled_triangle(x - 5, y + 24, x, y + 32, x, y + 24, al_map_rgb(255, 255, 255)); //left leg
+			al_draw_filled_triangle(x + 4, y + 24, x, y + 30, x, y + 24, al_map_rgb(130, 130, 130)); //right leg
+
+			al_draw_filled_triangle(x, y, x - 20, y + 20, x + 20, y + 20, al_map_rgb(255, 0, 0)); //dress
+			al_draw_filled_ellipse(x, y + 20, 16, 4, al_map_rgb(255, 0, 0)); // rounded dress bottom
+			al_draw_spline(dress_points, al_map_rgb(255, 255, 255), 1); // dress thread
+			al_draw_line(x - 6, y + 6, x + 5, y + 6, al_map_rgb(255, 255, 255), 2);//waist ribbon
+
+			al_draw_filled_triangle(x - 6, y - 4, x - 21, y, x - 18, y + 12, al_map_rgb(255, 255, 255)); //left arm
+			al_draw_filled_triangle(x + 6, y - 4, x + 21, y, x + 18, y + 12, al_map_rgb(255, 255, 255)); //right arm
+			al_draw_line(x - 20, y, x - 16, y + 10, al_map_rgb(255, 0, 0), 2); //left arm thread
+			al_draw_line(x + 20, y, x + 16, y + 10, al_map_rgb(255, 0, 0), 2); //right arm thread
+
+			al_draw_filled_triangle(x, y - 18, x - 16, y - 24, x - 12, y - 8, al_map_rgb(255, 0, 0)); //left ribbon
+			al_draw_filled_triangle(x, y - 18, x + 16, y - 24, x + 12, y - 8, al_map_rgb(255, 0, 0)); //right ribbon
+			al_draw_line(x - 16, y - 24, x - 12, y - 8, al_map_rgb(255, 255, 255), 2); //left ribbon frill
+			al_draw_line(x + 16, y - 24, x + 12, y - 8, al_map_rgb(255, 255, 255), 2); //right ribbon frill
+			al_draw_line(x - 2, y - 18, x + 2, y - 18, al_map_rgb(255, 0, 0), 3); //ribbon knot
+
+			al_draw_line(x + 20, y + 20, x + 28, y - 30, al_map_rgb(139, 69, 19), 2); //gohei stick
+			al_draw_filled_rectangle(x + 28, y - 30, x + 30, y - 25, al_map_rgb(255, 255, 255)); //gohei white pattern
+			al_draw_filled_rectangle(x + 30, y - 25, x + 32, y - 20, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 28, y - 20, x + 30, y - 15, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 30, y - 15, x + 32, y - 10, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 28, y - 10, x + 30, y - 5, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 30, y - 5, x + 32, y, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 28, y, x + 30, y + 5, al_map_rgb(255, 255, 255));
+			al_draw_filled_rectangle(x + 30, y + 5, x + 32, y + 10, al_map_rgb(255, 255, 255));
+		}
+
 	}
 }
 void reimuClass::erase_reimu()
